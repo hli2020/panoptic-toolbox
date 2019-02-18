706b31416ae6ab038ff70a4f65278694b4d5b312
hyli2020 <francislee2020@gmail.com>
2019-02-18 17:33:39 +0800

    update

---
+++ scripts/hyli_getData.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script downloads videos for a specific sequence:
+# ./getData.sh [sequenceName] [numVGAViews] [numHDViews]
+#
+# e.g., to download 10 VGA camera views for the "sampleData" sequence:
+# ./getData.sh sampleData 10 0
+#
+
+./getData.sh 160422_ultimatum1 0
